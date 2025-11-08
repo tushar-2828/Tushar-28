c7570bca3d77dfe655cf4dc31c9b45a13d9f1182
tushar-2828 <tk947770@gmail.com>
2025-11-08 15:12:55 +0530

    Add files via upload

---
+++ Calculator1.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('inputBox');
+let btns = document.querySelectorAll("button");
+
+let string = '';
+let buttonsArray = Array.from(btns);
+
+buttonsArray.forEach(btns => {
+    btns.addEventListener("click", (e) => {
+
+        if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            display.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = '';
+            display.value = string;
+        }
+        else if(e.target.innerHTML == '='){
+            string = eval(string);
+            display.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+    });
+});
